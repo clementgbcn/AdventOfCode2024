cee67eb1c2fca811be66f0b9ea1dc0a454cd980b
Clement Gaboriau Couanau <clement.gaboriaucouanau@datadoghq.com>
2024-12-07 08:21:15 +0100

    [DAY-07] Improve solutions of the 2 stars of Day 7

--- days/day_07.py
+++ days/day_07.py
@@ -1,4 +1,3 @@
-from functools import reduce
 from typing import List
 
 from day_factory.day import Day
@@ -14,47 +13,15 @@ class Day07(Day):
         super().__init__(self)
 
     @staticmethod
-    def compute_calibration(input_value: List[str]) -> int:
-        total = 0
-        for line in input_value:
-            v = extract_int(line)
-            if reduce(lambda x, y: x + y if min(x, y) == 1 else x * y, v[1:]) < v[0]:
-                continue
-            for comb in range(0, 2 ** (len(v) - 2)):
-                calibration = v[1]
-                for i in range(0, len(v) - 2):
-                    calibration = (
-                        calibration + v[i + 2]
-                        if comb & (1 << i)
-                        else calibration * v[i + 2]
-                    )
-                    if calibration > v[0]:
-                        break
-                if calibration == v[0]:
-                    total += v[0]
-                    break
-        return total
-
-    @staticmethod
-    def compute_calibration_fast(input_value: List[str]) -> int:
-        total = 0
-        for line in input_value:
-            v = extract_int(line)
-            print(v)
-            if Day07.compute_recursive(v[0], v[1:], False):
-                total += v[0]
-        return total
-
-    @staticmethod
     def compute_recursive(target: int, v, support_pipe=False) -> bool:
         if len(v) == 1:
             return v[0] == target
-        if target > v[-1] and Day07.compute_recursive(
-            target - v[-1], v[:-1], support_pipe
-        ):
-            return True
-        if target % v[-1] == 0 and Day07.compute_recursive(
-            target // v[-1], v[:-1], support_pipe
+        if (
+            target > v[-1]
+            and Day07.compute_recursive(target - v[-1], v[:-1], support_pipe)
+        ) or (
+            target % v[-1] == 0
+            and Day07.compute_recursive(target // v[-1], v[:-1], support_pipe)
         ):
             return True
         if support_pipe:
@@ -69,44 +36,16 @@ class Day07(Day):
         return False
 
     @staticmethod
-    def compute_calibration_2(input_value: List[str]) -> int:
+    def compute_calibration(input_value: List[str], support_pipe=False) -> int:
         total = 0
         for line in input_value:
             v = extract_int(line)
-            calibration = v[1]
-            for i in range(0, len(v) - 2):
-                calibration = int(str(calibration) + str(v[i + 2]))
-            if calibration < v[0]:
-                continue
-            for comb in range(0, 3 ** (len(v) - 2)):
-                calibration = v[1]
-                product = comb
-                for i in range(0, len(v) - 2):
-                    if product % 3 == 0:
-                        calibration *= v[i + 2]
-                    elif product % 3 == 1:
-                        calibration += v[i + 2]
-                    else:
-                        calibration = int(str(calibration) + str(v[i + 2]))
-                    if calibration > v[0]:
-                        break
-                    product = product // 3
-                if calibration == v[0]:
-                    total += v[0]
-                    break
-        return total
-
-    @staticmethod
-    def compute_calibration_2_fast(input_value: List[str]) -> int:
-        total = 0
-        for line in input_value:
-            v = extract_int(line)
-            if Day07.compute_recursive(v[0], v[1:], True):
+            if Day07.compute_recursive(v[0], v[1:], support_pipe):
                 total += v[0]
         return total
 
     def solution_first_star(self, input_value: List[str], input_type: TestEnum) -> int:
-        return self.compute_calibration(input_value)
+        return self.compute_calibration(input_value, False)
 
     def solution_second_star(self, input_value: List[str], input_type: TestEnum) -> int:
-        return self.compute_calibration_2(input_value)
+        return self.compute_calibration(input_value, True)
