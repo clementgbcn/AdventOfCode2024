18fed6cc6d7af90edf3d84f02b7c7a4932b51af2
Clement Gaboriau Couanau <clement.gaboriaucouanau@datadoghq.com>
2024-12-15 21:23:45 +0100

    [DAY-15] Add viz to Solutions of the 2 stars of Day 15

--- days/day_15.py
+++ days/day_15.py
@@ -1,10 +1,14 @@
 import logging
+import curses
+from time import sleep
 
 from day_factory.day import Day
 from day_factory.day_utils import TestEnum
 from utils.input_parser import InputParser
 
 logger = logging.getLogger(__name__)
+
+DISPLAY_BOT = False
 
 
 class Day15(Day):
@@ -15,9 +19,53 @@ class Day15(Day):
 
     def __init__(self):
         super().__init__(self)
+        if DISPLAY_BOT:
+            self.stdscr = curses.initscr()
+            curses.noecho()
+            curses.cbreak()
+            curses.start_color()
+            curses.use_default_colors()
+            for i in range(0, curses.COLORS - 1):
+                curses.init_pair(i + 1, i, -1)
+        else:
+            self.stdscr = None
 
-    @staticmethod
-    def compute_robots(input_value: InputParser) -> int:
+    def display(
+        self, warehouse, max_i, max_j, iteration, move, can_move, boxes, updated
+    ):
+        self.stdscr.addstr(0, 0, f"Iteration: {iteration}")
+        self.stdscr.addstr(1, 0, f"Move: {move}")
+        for j in range(max_j):
+            for i in range(max_i):
+                color = curses.COLOR_WHITE
+                if warehouse[i + j * 1j] == "@":
+                    if can_move:
+                        color = (
+                            curses.color_pair(47)
+                            if len(boxes) > 0
+                            else curses.color_pair(227)
+                        )
+                    else:
+                        color = curses.color_pair(197)
+                elif warehouse[i + j * 1j] == "[":
+                    if can_move and i + j * 1j in updated:
+                        color = curses.color_pair(47)
+                    elif not can_move and (
+                        i + j * 1j in boxes or i + 1 + j * 1j in boxes
+                    ):
+                        color = curses.color_pair(197)
+                elif warehouse[i + j * 1j] == "]":
+                    if can_move and i - 1 + j * 1j in updated:
+                        color = curses.color_pair(47)
+                    elif not can_move and (
+                        i - 1 + j * 1j in boxes or i + j * 1j in boxes
+                    ):
+                        color = curses.color_pair(197)
+                self.stdscr.addstr(2 + j, i, warehouse[i + j * 1j], color)
+        self.stdscr.refresh()
+        sleep(1 / 24)
+
+    def compute_robots(self, input_value: InputParser) -> int:
         warehouse = {}
         moves = []
         iterator = input_value.get_iterator()
@@ -34,7 +82,7 @@ class Day15(Day):
         max_j = j
         for line in iterator:
             moves += list(line)
-        for m in moves:
+        for iteration, m in enumerate(moves):
             d = Day15.DIRECTION[m]
             boxes = []
             can_move = True
@@ -52,6 +100,10 @@ class Day15(Day):
                 warehouse[robot] = "."
                 robot = robot + d
                 warehouse[robot] = "@"
+            if DISPLAY_BOT:
+                self.display(
+                    warehouse, max_i, max_j, iteration, m, can_move, set(), set()
+                )
         count = 0
         for j in range(max_j):
             for i in range(max_i):
@@ -59,8 +111,7 @@ class Day15(Day):
                     count += j * 100 + i
         return count
 
-    @staticmethod
-    def compute_robots_2(input_value: InputParser) -> int:
+    def compute_robots_2(self, input_value: InputParser) -> int:
         warehouse = {}
         moves = []
         iterator = input_value.get_iterator()
@@ -84,10 +135,11 @@ class Day15(Day):
         max_j = j
         for line in iterator:
             moves += list(line)
-        for m in moves:
+        for iteration, m in enumerate(moves):
             d = Day15.DIRECTION[m]
             boxes = set()
             visited = set()
+            updated = set()
             bfs = [robot]
             can_move = True
             while len(bfs):
@@ -109,7 +161,6 @@ class Day15(Day):
                     can_move = False
                     break
             if can_move:
-                updated = set()
                 for b in boxes:
                     warehouse[b + d] = "["
                     warehouse[b + 1 + d] = "]"
@@ -123,6 +174,10 @@ class Day15(Day):
                 warehouse[robot] = "."
                 robot = robot + d
                 warehouse[robot] = "@"
+            if DISPLAY_BOT:
+                self.display(
+                    warehouse, max_i, max_j, iteration, m, can_move, boxes, updated
+                )
         count = 0
         for j in range(max_j):
             for i in range(max_i):
